46b0addd20e6b459995f01aee96ddd23dfbe3c11
Can Berkol <can.berkol@biberltd.com>
2017-05-21 23:34:36 +0300

    comments fixed

--- Services/PhpOrient.php
+++ Services/PhpOrient.php
@@ -1,18 +1,5 @@
 <?php
-/**
- * @vendor      BiberLtd
- * @package		Core\Bundles\PhpOrientBundle
- * @subpackage	Services
- * @name	    PhpOrientWrapper
- *
- * @author		Can Berkol
- *
- * @copyright   Biber Ltd. www.biberltd.com (C) 2015
- *
- * @version     1.0.2
- * @date        11.06.2015
- *
- */
+
 namespace BiberLtd\Bundle\PhpOrientBundle\Services;
 
 use Symfony\Component\DependencyInjection\ContainerInterface as Container;
@@ -32,20 +19,14 @@ class PhpOrient
 	 */
 	protected $orientParams;
 
-	/**
-	 * @param Container $container
-	 * @param $hostname
-	 * @param $port
-	 * @param $token
-	 *
-	 *
-	 * @since           1.0.0
-	 * @version         1.0.1
-	 *
-	 * @author			Can Berkol
-	 * @author			Ali Cavdar
-	 */
-	public function __construct(Container $container, $hostname, $port, $token)
+    /**
+     * PhpOrient constructor.
+     * @param Container $container
+     * @param string $hostname
+     * @param int $port
+     * @param $token
+     */
+	public function __construct(Container $container, string $hostname, int $port, $token)
 	{
 		$this->orientParams = $container->getParameter('orientdb');
 
@@ -59,231 +40,120 @@ class PhpOrient
 		$this->driver->password = $this->orientParams['root']['password'];
 	}
 
-	/**
-	 * @name            command()
-	 *
-	 * @since           1.0.0
-	 * @version         1.0.0
-	 *
-	 * @author          Can Berkol
-	 *
-	 * @param			string		$query
-	 *
-	 * @return 			mixed
-	 */
-	public function command($query)
+    /**
+     * @param string $query
+     * @return mixed
+     */
+	public function command(string $query)
 	{
 		return $this->driver->command($query);
 	}
 
-	/**
-	 * @name            connect()
-	 *
-	 * @since           1.0.0
-	 * @version         1.0.0
-	 *
-	 * @author          Can Berkol
-	 * @author			Ali Cavdar
-	 *
-	 * @param			string		$serializationType
-	 *
-	 * @return 			mixed
-	 */
+    /**
+     * @param $serializationType
+     * @return mixed
+     */
 	public function connect($serializationType = OrientConstants::SERIALIZATION_DOCUMENT2CSV)
 	{
 		return $this->driver->connect($this->driver->username, $this->driver->password, $serializationType);
 	}
 
-	/**
-	 * @name            dataClusterAdd()
-	 *
-	 * @since           1.0.0
-	 * @version         1.0.0
-	 *
-	 * @author          Can Berkol
-	 *
-	 * @$param 			string		$clusterName
-	 *                  string		$clusterType
-	 *
-	 * @return 			integer
-	 */
+    /**
+     * @param $clusterName
+     * @param $clusterType
+     * @return mixed
+     */
 	public function dataClusterAdd($clusterName, $clusterType = OrientConstants::CLUSTER_TYPE_PHYSICAL){
 		return $this->driver->dataClusterAdd($clusterName, $clusterType);
 	}
 
-	/**
-	 * @name            dataClusterCount()
-	 *
-	 * @since           1.0.0
-	 * @version         1.0.0
-	 *
-	 * @author          Can Berkol
-	 *
-	 * @$param 			array 		$clusterIds
-	 *
-	 * @return 			array
-	 */
+    /**
+     * @param array $clusterIds
+     * @return mixed
+     */
 	public function dataClusterCount(array $clusterIds = []){
 		return $this->driver->dataClusterCount($clusterIds);
 	}
 
-	/**
-	 * @name            dataClusterDataRange()
-	 *
-	 * @since           1.0.0
-	 * @version         1.0.0
-	 *
-	 * @author          Can Berkol
-	 *
-	 * @$param 			integer 		$clusterId
-	 *
-	 * @return 			array
-	 */
+    /**
+     * @param $clusterId
+     * @return mixed
+     */
 	public function dataClusterDataRange($clusterId){
 		return $this->driver->dataClusterDataRange($clusterId);
 	}
 
-	/**
-	 * @name            dataClusterDrop()
-	 *
-	 * @since           1.0.0
-	 * @version         1.0.0
-	 *
-	 * @author          Can Berkol
-	 *
-	 * @$param 			integer 		$clusterId
-	 *
-	 * @return 			bool
-	 */
+    /**
+     * @param $clusterId
+     * @return mixed
+     */
 	public function dataClusterDrop($clusterId){
 		return $this->driver->dataClusterDrop($clusterId);
 	}
 
-	/**
-	 * @name            dbClose()
-	 *
-	 * @since           1.0.0
-	 * @version         1.0.0
-	 *
-	 * @author          Can Berkol
-	 *
-	 * @return 			integer
-	 */
+    /**
+     * @return mixed
+     */
 	public function dbClose(){
 		return $this->driver->dbClose();
 	}
 
-	/**
-	 * @name            dbCountRecords()
-	 *
-	 * @since           1.0.0
-	 * @version         1.0.0
-	 *
-	 * @author          Can Berkol
-	 *
-	 * @return 			integer
-	 */
+    /**
+     * @return mixed
+     */
 	public function dbCountRecords(){
 		return $this->driver->dbCountRecords();
 	}
 
-	/**
-	 * @name            dbCreate()
-	 *
-	 * @since           1.0.0
-	 * @version         1.0.0
-	 *
-	 * @author          Can Berkol
-	 *
-	 * @param 			string		$database
-	 * @param			string		$storageType
-	 * @param			string		$databaseType
-	 *
-	 * @return 			bool
-	 */
+    /**
+     * @param $database
+     * @param $storageType
+     * @param $databaseType
+     * @return mixed
+     */
 	public function dbCreate($database, $storageType = OrientConstants::STORAGE_TYPE_PLOCAL, $databaseType = OrientConstants::DATABASE_TYPE_GRAPH){
 		return $this->driver->dbCreate($database, $storageType, $databaseType);
 	}
 
-	/**
-	 * @name            dbDrop()
-	 *
-	 * @since           1.0.0
-	 * @version         1.0.0
-	 *
-	 * @author          Can Berkol
-	 *
-	 * @param 						$database
-	 * @param			string		$storageType
-	 *
-	 * @return 			true
-	 */
+    /**
+     * @param $database
+     * @param $storageType
+     * @return mixed
+     */
 	public function dbDrop($database, $storageType = OrientConstants::STORAGE_TYPE_PLOCAL){
 		return $this->driver->dbDrop($database, $storageType);
 	}
 
-	/**
-	 * @name            dbExists()
-	 *
-	 * @since           1.0.0
-	 * @version         1.0.0
-	 *
-	 * @author          Can Berkol
-	 *
-	 * @param 			string		$database
-	 * @param			string		$databaseType
-	 *
-	 * @return 			bool
-	 */
+    /**
+     * @param $database
+     * @param $databaseType
+     * @return mixed
+     */
 	public function dbExists($database, $databaseType = OrientConstants::DATABASE_TYPE_GRAPH){
 		return $this->driver->dbExists($database, $databaseType);
 	}
 
-	/**
-	 * @name            dbFreeze()
-	 *
-	 * @since           1.0.0
-	 * @version         1.0.0
-	 *
-	 * @author          Can Berkol
-	 *
-	 * @param 			string		$dbName
-	 * @param			string		$storageType
-	 *
-	 * @return 			bool
-	 */
+    /**
+     * @param $dbName
+     * @param $storageType
+     * @return mixed
+     */
 	public function dbFreeze($dbName, $storageType = OrientConstants::STORAGE_TYPE_PLOCAL){
 		return $this->driver->dbFreeze($dbName, $storageType);
 	}
 
-	/**
-	 * @name            dbList()
-	 *
-	 * @since           1.0.0
-	 * @version         1.0.0
-	 *
-	 * @author          Can Berkol
-	 *
-	 * @return 			array
-	 */
+    /**
+     * @return mixed
+     */
 	public function dbList(){
 		return $this->driver->dbList();
 	}
 
-	/**
-	 * @name            dbOpen()
-	 *
-	 * @since           1.0.0
-	 * @version         1.0.1
-	 *
-	 * @author          Can Berkol
-	 * @author			Ali Cavdar
-	 *
-	 * @param			string		$database
-	 * @param			array		$params
-	 *
-	 * @return 			Orient\Protocols\Common\ClusterMap
-	 */
+    /**
+     * @param $database
+     * @param array $params
+     * @return mixed
+     */
 	public function dbOpen($database, array $params = [])
 	{
 		if(count($params) < 1){
@@ -298,393 +168,175 @@ class PhpOrient
 		return $this->driver->dbOpen($database, $username, $password, $params);
 	}
 
-	/**
-	 * @name            dbRelease()
-	 *
-	 * @since           1.0.0
-	 * @version         1.0.0
-	 *
-	 * @author          Can Berkol
-	 *
-	 * @param			string		$dbName
-	 * @param			string		$storageType
-	 *
-	 * @return 			true
-	 */
+    /**
+     * @param $dbName
+     * @param $storageType
+     * @return mixed
+     */
 	public function dbRelease($dbName, $storageType = OrientConstants::STORAGE_TYPE_PLOCAL){
 		return $this->driver->dbRelease($dbName, $storageType);
 	}
 
-	/**
-	 * @name            dbReload()
-	 *
-	 * @since           1.0.0
-	 * @version         1.0.0
-	 *
-	 * @author          Can Berkol
-	 *
-	 * @return 			Orient\Protocols\Common\ClusterMap
-	 */
+    /**
+     * @return mixed
+     */
 	public function dbReload(){
 		return $this->driver->dbReload();
 	}
 
-	/**
-	 * @name            dbSize()
-	 *
-	 * @since           1.0.0
-	 * @version         1.0.0
-	 *
-	 * @author          Can Berkol
-	 *
-	 * @return 			integer|string
-	 */
+    /**
+     * @return mixed
+     */
 	public function dbSize(){
 		return $this->driver->dbSize();
 	}
-	/**
-	 * @name            execute()
-	 *
-	 * @since           1.0.0
-	 * @version         1.0.0
-	 *
-	 * @author          Can Berkol
-	 *
-	 * @param			string		$operation
-	 * @param			array		$params
-	 *
-	 * @return 			mixed
-	 */
 
+    /**
+     * @param $operation
+     * @param array $params
+     * @return mixed
+     */
 	public function execute($operation, array $params = []){
 		return $this->driver->execute($operation, $params);
 	}
 
-	/**
-	 * @name            getNewInstance()
-	 *
-	 * @since           1.0.0
-	 * @version         1.0.0
-	 *
-	 * @author          Can Berkol
-	 *
-	 * @param			string		$hostname
-	 * @param			string		$port
-	 * @param			string|bool	$token
-	 *
-	 * @return 			Orient\PhoOrient
-	 */
+    /**
+     * @param string $hostname
+     * @param string $port
+     * @param string $token
+     * @return Orient\PhpOrient
+     */
 	public function getNewInstance($hostname = '', $port = '', $token = ''){
 		$this->driver = new Orient\PhpOrient($hostname, $port, $token);
 		return $this->driver;
 	}
 
-	/**
-	 * @name            getSessionToken()
-	 *
-	 * @since           1.0.0
-	 * @version         1.0.0
-	 *
-	 * @author          Can Berkol
-	 *
-	 * @return 			string
-	 */
+    /**
+     * @return mixed
+     */
 	public function getSessionToken(){
-		return $this->driver->getessionToken();
+		return $this->driver->getSessionToken();
 	}
 
-	/**
-	 * @name            getTransport()
-	 *
-	 * @since           1.0.0
-	 * @version         1.0.0
-	 *
-	 * @author          Can Berkol
-	 *
-	 * @return 			Orient\Protocols\Binary\SocketTransport
-	 */
+    /**
+     * @return mixed
+     */
 	public function getTransport(){
 		return $this->driver->getTransport();
 	}
 
-	/**
-	 * @name            getTransactionStatement()
-	 *
-	 * @since           1.0.0
-	 * @version         1.0.0
-	 *
-	 * @author          Can Berkol
-	 *
-	 * @return 			Orient\Protocols\Binary\Transaction\TxCommit
-	 */
+    /**
+     * @return mixed
+     */
 	public function getTransactionStatement(){
 		return $this->driver->getTransactionStatement();
 	}
 
-	/**
-	 * @name            setHostname()
-	 *
-	 * @since           1.0.1
-	 * @version         1.0.1
-	 *
-	 * @author          Can Berkol
-	 *
-	 * @param			string		$hostname
-	 *
-	 * @return 			Orient\PhoOrient
-	 */
+    /**
+     * @param $hostname
+     * @return mixed
+     */
 	public function setHostname($hostname){
 		return $this->driver->hostname = $hostname;
 	}
 
-	/**
-	 * @name            setPassword()
-	 *
-	 * @since           1.0.1
-	 * @version         1.0.1
-	 *
-	 * @author          Can Berkol
-	 *
-	 * @param			string		$password
-	 *
-	 * @return 			Orient\PhoOrient
-	 */
+    /**
+     * @param $password
+     * @return mixed
+     */
 	public function setPassword($password){
 		return $this->driver->password = $password;
 	}
 
-	/**
-	 * @name            setPort()
-	 *
-	 * @since           1.0.1
-	 * @version         1.0.1
-	 *
-	 * @author          Can Berkol
-	 *
-	 * @param			integer		$port
-	 *
-	 * @return 			Orient\PhoOrient
-	 */
+    /**
+     * @param $port
+     * @return mixed
+     */
 	public function setPort($port){
 		return $this->driver->port = $port;
 	}
 
-	/**
-	 * @name            setTransport()
-	 *
-	 * @since           1.0.0
-	 * @version         1.0.0
-	 *
-	 * @author          Can Berkol
-	 *
-	 * @param			Orient\Protocols\Common\TransportInterface		$transport
-	 *
-	 * @return 			Orient\PhoOrient
-	 */
+    /**
+     * @param Orient\Protocols\Common\TransportInterface $transport
+     * @return mixed
+     */
 	public function setTransport(Orient\Protocols\Common\TransportInterface $transport){
 		return $this->driver->setTransport($transport);
 	}
 
-	/**
-	 * @name            setUserName()
-	 *
-	 * @since           1.0.1
-	 * @version         1.0.1
-	 *
-	 * @author          Can Berkol
-	 *
-	 * @param			string		$username
-	 *
-	 * @return 			Orient\PhoOrient
-	 */
+    /**
+     * @param $username
+     * @return mixed
+     */
 	public function setUserName($username){
 		return $this->driver->username = $username;
 	}
 
-	/**
-	 * @name            shutDown()
-	 *
-	 * @since           1.0.0
-	 * @version         1.0.0
-	 *
-	 * @author          Can Berkol
-	 *
-	 * @param			string		$username
-	 * @param			string		$password
-	 */
+    /**
+     * @param $username
+     * @param $password
+     */
 	public function shutDown($username, $password){
 		$this->driver->shutDown($username, $password);
 	}
 
-	/**
-	 * @name            sqlBatch()
-	 *
-	 * @since           1.0.0
-	 * @version         1.0.2
-	 *
-	 * @author          Can Berkol
-	 *
-	 * @param			string		$param
-	 *
-	 * @return 			mixed
-	 */
+    /**
+     * @param $param
+     * @return mixed
+     */
 	public function sqlBatch($param){
 		return $this->driver->sqlBatch($param);
 	}
 
-	/**
-	 * @name            query()
-	 *
-	 * @since           1.0.0
-	 * @version         1.0.0
-	 *
-	 * @author          Can Berkol
-	 *
-	 * @param			string		$query
-	 * @param			integer		$limit
-	 * @param			string		$fetchPlan
-	 *
-	 * @return			mixed
-	 */
+    /**
+     * @param $query
+     * @param int $limit
+     * @param string $fetchPlan
+     * @return mixed
+     */
 	public function query($query, $limit = 20, $fetchPlan = '*:0'){
 		return $this->driver->query($query, $limit, $fetchPlan);
 	}
 
-	/**
-	 * @name            queryAsync()
-	 *
-	 * @since           1.0.0
-	 * @version         1.0.0
-	 *
-	 * @author          Can Berkol
-	 *
-	 * @param			string		$query
-	 * @param			array		$params
-	 *
-	 * @return			mixed
-	 */
+    /**
+     * @param $query
+     * @param array $params
+     * @return mixed
+     */
 	public function queryAsync($query, array $params = []){
 		return $this->driver->queryAsync($query, $params);
 	}
 
-	/**
-	 * @name            recordCreate()
-	 *
-	 * @since           1.0.0
-	 * @version         1.0.0
-	 *
-	 * @author          Can Berkol
-	 *
-	 * @param			OrientData\Record		$record
-	 *
-	 * @return			OrientData\RecordCreate|OrientData\Record
-	 */
+    /**
+     * @param OrientData\Record $record
+     * @return mixed
+     */
 	public function recordCreate(OrientData\Record $record){
 		return $this->driver->recordCreate($record);
 	}
 
-	/**
-	 * @name            recordDelete()
-	 *
-	 * @since           1.0.0
-	 * @version         1.0.0
-	 *
-	 * @author          Can Berkol
-	 *
-	 * @param			OrientData\Id		$rid
-	 *
-	 * @return			OrientData\RecordCDelete|bool
-	 */
+    /**
+     * @param OrientData\Id $rid
+     * @return mixed
+     */
 	public function recordDelete(OrientData\Id $rid){
 		return $this->driver->recordDelete($rid);
 	}
 
-	/**
-	 * @name            recordLoad()
-	 *
-	 * @since           1.0.0
-	 * @version         1.0.0
-	 *
-	 * @author          Can Berkol
-	 *
-	 * @param			OrientData\Id		$rid
-	 * @param			array				$params
-	 *
-	 * @return			OrientData\recordLoad|OrientData\Record
-	 */
+    /**
+     * @param OrientData\Id $rid
+     * @param array $params
+     * @return mixed
+     */
 	public function recordLoad(OrientData\Id $rid, array $params = []){
 		return $this->driver->recordLoad($rid, $params);
 	}
 
-	/**
-	 * @name            recordUpdate()
-	 *
-	 * @since           1.0.0
-	 * @version         1.0.0
-	 *
-	 * @author          Can Berkol
-	 *
-	 * @param			OrientData\Record		$record
-	 *
-	 * @return			OrientData\RecordUpdate|OrientData\Record
-	 */
+    /**
+     * @param OrientData\Record $record
+     * @return mixed
+     */
 	public function recordUpdate(OrientData\Record $record){
 		return $this->driver->recordUpdate($record);
 	}
 }
-
-/**
- * Change Log
- * **************************************
- * v1.0.1                      11.06.2015
- * Can Berkol
- * **************************************
- * BF :: sqlBatch() method was calling batch() instead of sqlBatch(). Fixed.
- *
- * **************************************
- * v1.0.1                      03.06.2015
- * Can Berkol
- * **************************************
- * BF :: Namespace issues fixed.
- * FR :: setHostname() method added.
- * FR :: setPassword() method added.
- * FR :: setPort() method added.
- * FR :: setUsername() method added.
- *
- * **************************************
- * v1.0.0                      01.06.2015
- * Can Berkol
- * **************************************
- * FR :: __construct()
- * FR :: command()
- * FR :: connect()
- * FR :: dataClusterAdd()
- * FR :: dataClusterCount()
- * FR :: dataClusterDataRange()
- * FR :: dataClusterDrop()
- * FR :: dbClose()
- * FR :: dbCreate()
- * FR :: dbDrop()
- * FR :: dbExists()
- * FR :: dbFreeze()
- * FR :: dbList()
- * FR :: dbOpen()
- * FR :: dbRelease()
- * FR :: dbReload()
- * FR :: dbSize()
- * FR :: execute()
- * FR :: getNewInstance()
- * FR :: getSessionToken()
- * FR :: getTransport()
- * FR :: getTransactionStatement()
- * FR :: setSessionToken()
- * FR :: setTransport()
- * FR :: shutDown()
- * FR :: sqlBatch()
- * FR :: query()
- * FR :: queryAsync()
- * FR :: recordCreate()
- * FR :: recordDelete()
- * FR :: recordLoad()
- * FR :: recordUpdate()
- */
